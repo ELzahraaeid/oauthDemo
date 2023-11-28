928db3ef8ee2126c2c04aaf59ae0d1ab6e98d902
Alzahraa Eid <alzahraa.eid@fixedmea.com>
2023-11-28 16:58:39 +0200

    update2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import SingleFileUploader from './components/SingleFileUploader';
+import axios from 'axios';
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  useEffect(() => {
+    if (code) {
+      axios
+        .post('http://localhost:5000/tokenExchange', { code })
+        .then(({ data }) => console.log({ data }));
+    }
+  }, [code]);
   return (
     <div className="App">
       <header className="App-header">
